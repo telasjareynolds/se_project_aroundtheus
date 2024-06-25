e45136b7e655577f3da4516939605206c1adfb4f
Telasja <telasjareynolds@gmail.com>
2024-06-25 11:48:09 -0400

    first js commit

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+const initialCards = [
+  {
+    name: "Delijan, Iran",
+    link: "https://images.unsplash.com/photo-1716102229255-faeed7ddb72a?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  // }, 
+  // {
+  //   name: 
+  //   link:
+  // },
+  // {
+  //   name: 
+  //   link:
+  // },
+  // {
+  //   name: 
+  //   link:
+  // },
+  // {
+  //   name: 
+  //   link:
+  // },
+  // {
+  //   name: 
+  //   link:
+  // },
+  // {
+  //   name: 
+  //   link:
+  // },
+  // {
+  //   name: 
+  //   link:
+ }
+];
+
+console.log(initialCards);
